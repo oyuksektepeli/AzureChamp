8447c6759dbd2703eee98343418a3f63b115baea
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-04-08 15:45:44 +0300

    Create AzureRMVMs.ps1

---
+++ AzureRMVMs.ps1
@@ -0,0 +1,11 @@
+#Start Azure VM With Powershell
+Start-AzureRmVM -ResourceGroupName "mac-dev-rg" -Name "mclinuxvm01"
+
+#Start Azure VM with Azure CLI
+az vm start --resource-group mac-dev-rg --name mclinuxvm01
+
+#Stop Azure VM with Powershell
+Stop-AzureRmVM -ResourceGroupName "mac-dev-rg" -Name "mclinuxvm01"
+
+#Stop Azure VM with Azure CLI
+az vm stop --resource-group mac-dev-rg --name mclinuxvm01
