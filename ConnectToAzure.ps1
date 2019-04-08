8f9b6da23e97591a382795310b6d208df3b9ce8e
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-04-08 15:19:34 +0300

    Update ConnectToAzure.ps1

--- ConnectToAzure.ps1
+++ ConnectToAzure.ps1
@@ -10,4 +10,16 @@ Get-AzureRmContext
 Get-AzRmSubscription | more
 
 #Select Azure Subscription
-Select-AzureRmSubscription -SubscriptionName <Insert Subscription Name>
+Select-AzureRmSubscription -SubscriptionName "Subscription Name"
+
+
+#Connect Azure with Azure CLI
+
+#Connect and Authenticate
+Az-login
+
+#Get All Azure Subscriptions
+az account list
+
+#Select Azure Subscription
+az account set --subscription "subscription-name"
