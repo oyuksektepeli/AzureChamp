c0e69e02589071a62df0a53183cab47d5281bdb4
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-04-08 15:36:22 +0300

    Update ResourceGroups.ps1

--- ResourceGroups.ps1
+++ ResourceGroups.ps1
@@ -3,23 +3,3 @@ New-Azurermresourcegroup -name "mac-prod-rg" -Location "West Europe"
 
 #Create Resource Group from Azure CLI
 Az group create --name "mac-dev-rg" --location "West Europe"
-
-#Get Tags of Resource Group
-(Get-AzureRMResourceGroup -Name mac-dev-rg).Tags
-
-
-#Add Tags to Resources with Powershell
-Set-AzureRMResourceGroup -Name mac-dev-rg -Tag @{ Dept="IT"; Owner="Onur Yuksektepeli"}
-
-#Add Tags to Existing List with Powershell
-$tags = (Get-AzureRMResourceGroup -Name mac-dev-rg).Tags
-$tags.add("CostCenter","ECommerce")
-Set-AzureRmResourceGroup -Tag $tags -name mac-dev-rg
-
-#Add Tags to Resources with Azure CLI
-Az group show -n mac-dev-rg --query tags
-Az group update -n mac-dev-rg --set tags.Owner=OnurYuksektepeli tags.Deparment=IT
-
-#Clear Resource Tags with Powershell
-Set-AzureRMResourceGroup -Tag @{} -Name mac-dev-rg
-
