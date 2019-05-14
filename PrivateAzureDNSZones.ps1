1d8d752f0feae873539d3631000b02291323fc00
oyuksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-05-14 17:38:02 +0300

    new code added

--- PrivateAzureDNSZones.ps1
+++ PrivateAzureDNSZones.ps1
@@ -1,23 +1,24 @@
-$rgName = "mac-dns-rg"
-$zoneName = "mac.local"
+$rgName = "mac-vnet-rg"
+$zoneName = "trainer.local"
 
-$macvnet1 = "macvnet1"
-$hostname1 = "mac-win2k19-01"
-$hostip1 = "10.1.1.4"
+$macvnet3 = "macvnet03"
+$hostname1 = "macwin2k1603"
+$hostip1 = "10.3.0.5"
 
-$macvnet2 = "macvnet2"
-$hostname2 ="mac-win2k19-02"
-$hostip2 = "10.2.1.4"
+$macvnet4 = "macvnet04"
+$hostname2 ="macwin2k1604"
+$hostip2 = "10.4.0.5"
 
-$vnet1 = Get-AzureRmVirtualNetwork -Name $macvnet1 -ResourceGroupName $rgName
-$vnet2 = Get-AzureRmVirtualNetwork -Name $macvnet2 -ResourceGroupName $rgName
+$vnet3 = Get-AzureRmVirtualNetwork -Name $macvnet3 -ResourceGroupName $rgName
+$vnet4 = Get-AzureRmVirtualNetwork -Name $macvnet4 -ResourceGroupName $rgName
 
-New-AzureRmDnsZone -Name $zoneName -ResourceGroupName $rgName -ZoneType Private -ResolutionVirtualNetworkId @($vnet1.Id)
+New-AzureRmDnsZone -Name $zoneName -ResourceGroupName $rgName -ZoneType Private -ResolutionVirtualNetworkId @($vnet3.Id)
+New-AzureRmDnsZone -Name "trainer2.local" -ResourceGroupName $rgName -ZoneType Private
 
 New-AzureRmDnsRecordSet -Name $hostname1 -RecordType A -ZoneName $zoneName -ResourceGroupName $rgName -Ttl 3600
 New-AzureRmDnsRecordSet -Name $hostname2 -RecordType A -ZoneName $zoneName -ResourceGroupName $rgName  -Ttl 3600
 
 Get-AzureRmDnsRecordSet -ZoneName $zoneName -ResourceGroupName $rgName
 
-Set-AzureRmDnsZone -Name $zoneName -ResourceGroupName $rgName -RegistrationVirtualNetworkId @($macvnet1.Id)
-Set-AzureRmDnsZone -Name $zoneName -ResourceGroupName $rgName -ResolutionVirtualNetworkId @($macvnet2.Id)
+Set-AzureRmDnsZone -Name $zoneName -ResourceGroupName $rgName -RegistrationVirtualNetworkId @($vnet3.Id)
+Set-AzureRmDnsZone -Name $zoneName -ResourceGroupName $rgName -ResolutionVirtualNetworkId @($vnet4.Id)
