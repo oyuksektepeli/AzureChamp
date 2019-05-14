afc3a2b8e835809c5ca6530f75dba8bf79f0310d
oyuksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-05-14 22:13:10 +0300

    script edited

--- PrivateAzureDNSZones.ps1
+++ PrivateAzureDNSZones.ps1
@@ -1,24 +1,24 @@
-$rgName = "mac-vnet-rg"
-$zoneName = "trainer.local"
+$rgName = "mac-network-rg"
+$zoneName = "mac.local"
 
-$macvnet3 = "macvnet03"
-$hostname1 = "macwin2k1603"
-$hostip1 = "10.3.0.5"
+$macvnet10 = "macvnet10"
+$hostname10 = "macwin2k1610"
+$hostip1 = "10.10.10.4"
 
-$macvnet4 = "macvnet04"
-$hostname2 ="macwin2k1604"
-$hostip2 = "10.4.0.5"
+$macvnet20 = "macvnet20"
+$hostname20 ="macwin2k1620"
+$hostip2 = "10.20.20.4"
 
-$vnet3 = Get-AzureRmVirtualNetwork -Name $macvnet3 -ResourceGroupName $rgName
-$vnet4 = Get-AzureRmVirtualNetwork -Name $macvnet4 -ResourceGroupName $rgName
+$vnet10 = Get-AzureRmVirtualNetwork -Name $macvnet10 -ResourceGroupName $rgName
+$vnet20 = Get-AzureRmVirtualNetwork -Name $macvnet20 -ResourceGroupName $rgName
 
-New-AzureRmDnsZone -Name $zoneName -ResourceGroupName $rgName -ZoneType Private -ResolutionVirtualNetworkId @($vnet3.Id)
+New-AzureRmDnsZone -Name $zoneName -ResourceGroupName $rgName -ZoneType Private -ResolutionVirtualNetworkId @($vnet40.Id)
 New-AzureRmDnsZone -Name "trainer2.local" -ResourceGroupName $rgName -ZoneType Private
 
-New-AzureRmDnsRecordSet -Name $hostname1 -RecordType A -ZoneName $zoneName -ResourceGroupName $rgName -Ttl 3600
-New-AzureRmDnsRecordSet -Name $hostname2 -RecordType A -ZoneName $zoneName -ResourceGroupName $rgName  -Ttl 3600
+New-AzureRmDnsRecordSet -Name $hostname10 -RecordType A -ZoneName $zoneName -ResourceGroupName $rgName -Ttl 3600
+New-AzureRmDnsRecordSet -Name $hostname20 -RecordType A -ZoneName $zoneName -ResourceGroupName $rgName  -Ttl 3600
 
 Get-AzureRmDnsRecordSet -ZoneName $zoneName -ResourceGroupName $rgName
 
 Set-AzureRmDnsZone -Name $zoneName -ResourceGroupName $rgName -RegistrationVirtualNetworkId @($vnet3.Id)
-Set-AzureRmDnsZone -Name $zoneName -ResourceGroupName $rgName -ResolutionVirtualNetworkId @($vnet4.Id)
+Set-AzureRmDnsZone -Name $zoneName -ResourceGroupName $rgName -ResolutionVirtualNetworkId @($vnet10.Id)
