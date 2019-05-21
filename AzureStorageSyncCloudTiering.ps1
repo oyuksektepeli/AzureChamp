b6b329e561dd3764fcf0404dad99f6ad586f88fa
oyuksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-05-21 16:07:05 +0300

    code added

--- AzureStorageSyncCloudTiering.ps1
+++ AzureStorageSyncCloudTiering.ps1
@@ -1,3 +1,12 @@
+#Import Storage Space Powershell Module
 import-module "C:\Program Files\Azure\StorageSyncAgent\StorageSync.Management.ServerCmdlets.dll"
+
+#Trigger Cloud Tiering Manually
 Inovoke-StorageSyncCloudTiering -Path "F:\AzureChamp\cdnfile01.txt"
+
+#Recall all files to file server
 Invoke-StorageSyncFileRecall -Path ""
+
+#File Sync QoS
+New-StorageSyncNetworkLimit -Day Monday,Tuesday -StartHour 1 -EndHour 7 -LimimitKbps 60000
+
