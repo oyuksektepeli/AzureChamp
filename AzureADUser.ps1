bf01c0bd6414c85526f55f3fea2c47e608ef6dfa
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-05-27 00:11:39 +0300

    azure ad user commands

---
+++ AzureADUser.ps1
@@ -0,0 +1,35 @@
+#Log into Azure
+Connect-AzureAD
+
+$domain = "ContosoNed.onmicrosoft.com"
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = "agxsFX72xwsSAi"
+
+#Find an existing user
+Get-AzureADUser -SearchString "Istanbul"
+
+Get-AzureADUser -Filter "State eq 'Istanbul'"
+
+#Creating a new user
+$user = @{
+    City = "Istanbul"
+    Country = "Turkey"
+    Department = "Information Technology"
+    DisplayName = "Haydar Pasa"
+    GivenName = "Haydar"
+    JobTitle = "IT Admin"
+    UserPrincipalName = "haydar.pasa@$domain"
+    PasswordProfile = $PasswordProfile
+    PostalCode = "34000"
+    State = "IST"
+    StreetAddress = "Haydar Pasa Caddesi"
+    Surname = "Pasa"
+    TelephoneNumber = "216-987-6544"
+    MailNickname = "Haydarpasa"
+    AccountEnabled = $true
+    UsageLocation = "TR"
+}
+
+$newUser = New-AzureADUser @user
+
+$newUser | Format-List
