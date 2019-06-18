9153c638f306a3b4244c4242e4ae3a1a15296d83
oyuksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-06-18 14:29:06 +0300

    container basics commands

---
+++ ContainerBasics.ps1
@@ -0,0 +1,47 @@
+#To see docker verison
+docker verison
+
+#detailed docker information
+docker info
+
+#to see local docker images
+docker images
+
+#search online images from dockerhub
+docker search ubuntu
+
+#download docker image
+docker pull ubuntu
+
+#create and run container from image 
+# 'd' for deattached mode 'it' for interactive mode
+docker run -d -p 8080:80 --name ubuntu1 ubuntu
+docker run it --name temp ubuntu
+
+#to see container logs
+docker logs ubuntu1
+
+#see running containers
+docker ps 
+
+#see all containers 
+docker ps -a
+
+#stop docker container
+docker stop ubuntu1
+
+#delete container 
+docker rm ubuntu1
+
+#delete container image
+docker rmi ubuntu
+docker image rm ubuntu
+
+#inspect docker image
+docker inspect ubuntu
+
+#to see docker image history
+docker history ubuntu
+
+
+
